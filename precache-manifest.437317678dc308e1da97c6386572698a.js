e09ba0f7df1b913d070c43d3489d20e9078e51cb
Mrozuu <58520713+Mrozuu@users.noreply.github.com>
2020-05-24 19:16:50 +0200

    Updates

---
+++ precache-manifest.437317678dc308e1da97c6386572698a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50184d0dca37f917046d819b6a76d43",
+    "url": "/PAULA/index.html"
+  },
+  {
+    "revision": "3da4a468c08de737128c",
+    "url": "/PAULA/static/css/main.121d3c5f.chunk.css"
+  },
+  {
+    "revision": "15905f474109aa4a8d24",
+    "url": "/PAULA/static/js/2.af6f2c02.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/PAULA/static/js/2.af6f2c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da4a468c08de737128c",
+    "url": "/PAULA/static/js/main.d7ab6744.chunk.js"
+  },
+  {
+    "revision": "d0c64ca83f3bbf8ef9d4",
+    "url": "/PAULA/static/js/runtime-main.371a8e45.js"
+  },
+  {
+    "revision": "7965382802bbc0cedb35801572354b16",
+    "url": "/PAULA/static/media/logo.79653828.png"
+  },
+  {
+    "revision": "35584cfaa7813f111f4fadecc35fea92",
+    "url": "/PAULA/static/media/slider_example1.35584cfa.jpg"
+  },
+  {
+    "revision": "f046d97bdedb8d715df4cbe82c9ce98a",
+    "url": "/PAULA/static/media/slider_example2.f046d97b.png"
+  }
+]);
