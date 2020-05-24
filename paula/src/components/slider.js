c1a8d43babdbb6bf02648b9141a88f2b9ff6024c
Mrozuu <58520713+Mrozuu@users.noreply.github.com>
2020-05-24 22:21:09 +0200

    drobne zmiany Slider

--- paula/src/components/slider.js
+++ paula/src/components/slider.js
@@ -9,15 +9,29 @@ class Slider extends React.Component {
         super();
         this.state = {
             currentImageIndex: 0,
-            images: [
-                image,
-                image2,
-                image,
-                image2,
-                image,
-                image2,
-                image,
-                image2
+            
+            sliderData : [
+                {
+                    url: image,
+                    title: "20 lat doświadczenia",
+                    description: "Lorem ipsum1"
+                },
+                {
+                    url: image2,
+                    title: "21 lat doświadczenia",
+                    description: "Lorem ipsum2"
+                },
+                {
+                    url: image,
+                    title: "22 lat doświadczenia",
+                    description: "Lorem ipsum3"
+                },                
+                {
+                    url: image2,
+                    title: "23 lat doświadczenia",
+                    description: "Lorem ipsum4"
+                },
+
             ],
             //SLIDER W SUMIE DZIAŁA, ALE TERAZ BĘDZIE TRZEBA TO TAK PRZEROBIĆ, ŻEBY
             //ZAMIAST SAMYCH ZDJ W IMAGES TO DZIAŁAŁO JAK OBIEKTY ŻEBY KAŻDE ZDJĘCIE
@@ -30,7 +44,7 @@ class Slider extends React.Component {
     }
 
     prevSlide() {
-        const lastIndex = this.state.images.length - 1;
+        const lastIndex = this.state.sliderData.length - 1;
 
         const resetIndex = this.state.currentImageIndex === 0;
         const index = resetIndex ? lastIndex : this.state.currentImageIndex - 1;
@@ -42,7 +56,7 @@ class Slider extends React.Component {
 
     nextSlide() {
         // find the index of the last image in the array
-        const lastIndex = this.state.images.length - 1;
+        const lastIndex = this.state.sliderData.length - 1;
         // check if we need to start over from the first index
         const resetIndex = this.state.currentImageIndex === lastIndex;
         const index = resetIndex ? 0 : this.state.currentImageIndex + 1;
@@ -52,14 +66,14 @@ class Slider extends React.Component {
         });
     }
     render() {
-        const { images } = this.state;
+        const { sliderData } = this.state;
         const index = this.state.currentImageIndex;
         return (
-            <div className="Slider" style={{ backgroundImage: `url(${images[index]})` }}>
+            <div className="Slider" style={{ backgroundImage: `url(${sliderData[index].url})` }}>
                 <img className="Slider__Arrow prevArrow" src={this.state.arrowPrev} onClick={this.prevSlide} />
                 <div className="Slider__box">
-                    <h2>20 lat doświadczenia</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <h2>{sliderData[index].title}</h2>
+                    <p>{sliderData[index].description}</p>
                 </div>
                 <img className="Slider__Arrow nextArrow" src={this.state.arrowNext} onClick={this.nextSlide} />
             </div>
