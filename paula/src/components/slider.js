b025e6360c64c0eb6fc140936e90b6cedf836a98
Mrozuu <58520713+Mrozuu@users.noreply.github.com>
2020-05-25 07:52:20 +0200

    Slider - działa przewijanie

--- paula/src/components/slider.js
+++ paula/src/components/slider.js
@@ -9,6 +9,8 @@ class Slider extends React.Component {
         super();
         this.state = {
             currentImageIndex: 0,
+
+            className : "Slider__elements Slider__transform",
             
             sliderData : [
                 {
@@ -33,9 +35,6 @@ class Slider extends React.Component {
                 },
 
             ],
-            //SLIDER W SUMIE DZIAŁA, ALE TERAZ BĘDZIE TRZEBA TO TAK PRZEROBIĆ, ŻEBY
-            //ZAMIAST SAMYCH ZDJ W IMAGES TO DZIAŁAŁO JAK OBIEKTY ŻEBY KAŻDE ZDJĘCIE
-            //WIEDZIAŁO CO MA WYŚWIETLAĆ ALE JAK NAJBARDZIEJ DO DOKOŃCZENIA
             arrowNext: arrowRight,
             arrowPrev: arrowLeft
         }
@@ -50,8 +49,14 @@ class Slider extends React.Component {
         const index = resetIndex ? lastIndex : this.state.currentImageIndex - 1;
 
         this.setState({
-            currentImageIndex: index
+            className : "Slider__elements Slider__go__left"
         })
+        setTimeout(() => {
+            this.setState({
+                currentImageIndex: index,
+                className : "Slider__elements Slider__transform"
+            })
+          }, 1000);
     }
 
     nextSlide() {
@@ -62,20 +67,47 @@ class Slider extends React.Component {
         const index = resetIndex ? 0 : this.state.currentImageIndex + 1;
         // assign the logical index to currentImageIndex that will use in render method
         this.setState({
-            currentImageIndex: index
-        });
+            className : "Slider__elements Slider__go__right"
+        })
+        setTimeout(() => {
+            this.setState({
+                currentImageIndex: index,
+                className : "Slider__elements Slider__transform"
+            })
+          }, 1000);
     }
     render() {
         const { sliderData } = this.state;
         const index = this.state.currentImageIndex;
+        const index_left = (index - 1 < 0 ? this.state.sliderData.length - 1 : index - 1);
+        const index_right = (index + 1 > this.state.sliderData.length -1 ? 0 : index + 1);        
         return (
-            <div className="Slider" style={{ backgroundImage: `url(${sliderData[index].url})` }}>
+            <div className="Slider">
                 <img className="Slider__Arrow prevArrow" src={this.state.arrowPrev} onClick={this.prevSlide} />
-                <div className="Slider__box">
-                    <h2>{sliderData[index].title}</h2>
-                    <p>{sliderData[index].description}</p>
-                </div>
                 <img className="Slider__Arrow nextArrow" src={this.state.arrowNext} onClick={this.nextSlide} />
+            <div className={this.state.className}>
+                    <div className="Slider__element Slider__left" style={{ backgroundImage: `url(${sliderData[index_left].url})` }}>
+                        <div className="Slider__box">
+                            <h2>{sliderData[index_left].title}</h2>
+                            <p>{sliderData[index_left].description}</p>
+                        </div>
+                </div>
+
+                    <div className="Slider__element Slider__center" style={{ backgroundImage: `url(${sliderData[index].url})` }}>
+                        <div className="Slider__box">
+                            <h2>{sliderData[index].title}</h2>
+                            <p>{sliderData[index].description}</p>
+                        </div>
+                    </div>
+
+
+                    <div className="Slider__element Slider__right" style={{ backgroundImage: `url(${sliderData[index_right].url})` }}>
+                        <div className="Slider__box">
+                            <h2>{sliderData[index_right].title}</h2>
+                            <p>{sliderData[index_right].description}</p>
+                        </div>
+                </div>
+            </div>
             </div>
         )
     }
