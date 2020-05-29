3a5b7a8c306456f042221b69799de574a99018da
kalllllor <karolgren3@gmail.com>
2020-05-29 20:52:23 +0200

    wstepna wersja aboutMe

--- paula/src/pages/AboutPage.js
+++ paula/src/pages/AboutPage.js
@@ -12,8 +12,22 @@ class AboutPage extends React.Component {
     render() {
 
         return (
-            <div className="mainPage">
+            <div className="aboutPage">
                 <Menu />
+
+                <div className="aboutPage__title" >
+                    <h1>O NAS</h1>
+                </div>
+
+                <div className="aboutPage__firstSection">
+                    <div className="firstSection__container">
+                        <div className="container__text">
+                            <h2>tytuł 1</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis, massa ut fringilla pharetra, est ligula semper tellus, in vulputate neque dolor a nisi. Donec eu purus purus. Fusce ultricies nunc metus, pharetra elementum est fermentum vel. Vestibulum non tempor ipsum. Duis at turpis sapien. Donec condimentum iaculis est id egestas. Nulla pulvinar quis neque eget congue. Aliquam ullamcorper, enim id mollis varius, dolor risus mollis diam, elementum euismod nisl orci non ipsum. Praesent imperdiet, orci a egestas tincidunt, urna risus malesuada ante, vitae mollis magna sapien sit amet ex. Fusce a turpis massa.</p>
+                        </div>
+                    </div>
+                    <div className="firstSection__container"></div>
+                </div>
                 <Footer />
             </div>
         )
