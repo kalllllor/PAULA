6fd2e5c8b3c67bb42ba0ed1c066067991ca5728d
kalllllor <karolgren3@gmail.com>
2020-05-30 19:20:56 +0200

    ContactPage wstepna wersja

--- paula/src/pages/AboutPage.js
+++ paula/src/pages/AboutPage.js
@@ -19,39 +19,41 @@ class AboutPage extends React.Component {
                 <div className="aboutPage">
 
                     <Menu active="aboutPage" />
-                    <div className="aboutPage__title" >
-                        <h1>O NAS</h1>
-                    </div>
-                    <div className="aboutPage__Section">
-                        <div className="Section__container container__underline">
-                            <div className="container__text">
-                                <div className="container__textTitle">
-                                    <h2>DOŚWIADCZENIE</h2>
+                    <div style={{ borderTop: "solid 3vh #FCC700" }}>
+                        <div className="aboutPage__title" >
+                            <h1>O NAS</h1>
+                        </div>
+                        <div className="aboutPage__Section">
+                            <div className="Section__container container__underline">
+                                <div className="container__text">
+                                    <div className="container__textTitle">
+                                        <h2>DOŚWIADCZENIE</h2>
+                                    </div>
+                                    <p>Od 1998 roku stanowimy istotny element dynamicznie rozwijającego się lokalnego rynku branży materiałów budowlanych. Od początku swojego istnienia stawiamy przede wszystkim na partnerstwo z firmami z sektora budowlanego, dzięki czemu jesteśmy dziś znanym i preferowanym dostawcą technologii i innowacyjnych rozwiązań w tej branży. Jako Firma Handlowa Paula zapewniamy obsługę firmom wykonawczym i prowadzimy także sprzedaż detaliczną.</p>
                                 </div>
-                                <p>Od 1998 roku stanowimy istotny element dynamicznie rozwijającego się lokalnego rynku branży materiałów budowlanych. Od początku swojego istnienia stawiamy przede wszystkim na partnerstwo z firmami z sektora budowlanego, dzięki czemu jesteśmy dziś znanym i preferowanym dostawcą technologii i innowacyjnych rozwiązań w tej branży. Jako Firma Handlowa Paula zapewniamy obsługę firmom wykonawczym i prowadzimy także sprzedaż detaliczną.</p>
                             </div>
-                        </div>
-                        <div className="Section__container">
-                            <div className="container__image">
-                                <img src={image} alt="" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="aboutPage__Section">
-                        <div className="Section__container">
-                            <div className="container__image">
-                                <img src={image2} alt="" />
-                            </div>
-                        </div>
-                        <div className="Section__container  container__underline">
-                            <div className="container__text">
-                                <div className="container__textTitle">
-                                    <h2>KONKURENCYJNE CENY</h2>
+                            <div className="Section__container">
+                                <div className="container__image">
+                                    <img src={image} alt="" />
                                 </div>
-                                <p>Mnogość artykułów, zróżnicowanie cenowe oraz fachowa i kompleksowa obsługa - to wizytówka naszej firmy, należącej do wiodących w branży. Znajdą Państwo tutaj różnorodne produkty najbardziej znanych producentów c Nasi konsultanci handlowi bardzo chętnie doradzą Państwu jakie materiały będą potrzebne, pomogą w ich doborze oraz objaśnią jak najlepsze zastosowanie. Zakupiony towar dostarczą na wskazane przez Państawa miejsce nasze samochody, dostawcze lub ciężarowe. Oferujemy możliwość zakupu towaru w systemie ratalnym, na bardzo korzystnych warunkach. Zamówienia towaru przyjmujemy także telefonicznie. Nasze składy budowlane mieszczą się w Słomnikach na ul. Proszowska 23 oraz w Miechowie na ul.Sienkiewicza 53.</p>
                             </div>
                         </div>
+                        <div className="aboutPage__Section">
+                            <div className="Section__container">
+                                <div className="container__image">
+                                    <img src={image2} alt="" />
+                                </div>
+                            </div>
+                            <div className="Section__container  container__underline">
+                                <div className="container__text">
+                                    <div className="container__textTitle">
+                                        <h2>KONKURENCYJNE CENY</h2>
+                                    </div>
+                                    <p>Mnogość artykułów, zróżnicowanie cenowe oraz fachowa i kompleksowa obsługa - to wizytówka naszej firmy, należącej do wiodących w branży. Znajdą Państwo tutaj różnorodne produkty najbardziej znanych producentów c Nasi konsultanci handlowi bardzo chętnie doradzą Państwu jakie materiały będą potrzebne, pomogą w ich doborze oraz objaśnią jak najlepsze zastosowanie. Zakupiony towar dostarczą na wskazane przez Państawa miejsce nasze samochody, dostawcze lub ciężarowe. Oferujemy możliwość zakupu towaru w systemie ratalnym, na bardzo korzystnych warunkach. Zamówienia towaru przyjmujemy także telefonicznie. Nasze składy budowlane mieszczą się w Słomnikach na ul. Proszowska 23 oraz w Miechowie na ul.Sienkiewicza 53.</p>
+                                </div>
+                            </div>
 
+                        </div>
                     </div>
                 </div>
                 <Footer />
--- paula/src/pages/ContactPage.js
+++ paula/src/pages/ContactPage.js
@@ -12,8 +12,59 @@ class ContactPage extends React.Component {
     render() {
 
         return (
-            <div className="mainPage">
-                <Menu active = "contactPage" />
+            <div className="contactPage">
+
+                <Menu active="contactPage" />
+                <div style={{ borderTop: "solid 3vh #FCC700" }}>
+                    <div className="contactPage__title" >
+                        <h1>KONTAKT</h1>
+                    </div>
+                    <div className="contactPage__content">
+                        <div className="contactPage__Section">
+                            <div className="container__title">
+                                <h2>ODDZIAŁ SŁOMNIKI</h2>
+                            </div>
+                            <div className="container__content">
+                                <div className="content__left">
+                                    <h2>GODZINY OTWARCIA</h2>
+                                    <p><h4>Poniedziałek:</h4> 8:00 - 18:00</p>
+                                    <p><h4>Wtorek:</h4> 8:00 - 18:00</p>
+                                    <p><h4>Środa:</h4> 8:00 - 18:00</p>
+                                    <p><h4>Czwartek:</h4> 8:00 - 18:00</p>
+                                    <p><h4>Piątek:</h4> 8:00 - 18:00</p>
+                                    <p><h4>Sobota:</h4> 8:00 - 18:00</p>
+                                </div>
+                                <div className="content__right">
+                                    <h3>ul. Proszowska 23</h3>
+                                    <h3>tel. 12 388 30 62</h3>
+                                    <h3>tel. 12 388 03 40</h3>
+                                </div>
+                            </div>
+                            <iframe className="contact__map" title="oddzialMiechow" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2948.1995532974543!2d20.084276602770252!3d50.24011881618411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47165294cab2fbf9%3A0x92062f2cf9df58b1!2sProszowska%2023%2C%2032-090%20S%C5%82omniki!5e0!3m2!1spl!2spl!4v1590180122445!5m2!1spl!2spl" />
+                        </div>
+                        <div className="contactPage__Section">
+                            <div className="container__title">
+                                <h2>ODDZIAŁ MIECHÓW</h2>
+                            </div>
+                            <div className="container__content">
+                                <div className="content__left">
+                                    <h2>GODZINY OTWARCIA</h2>
+                                    <p><h4>Poniedziałek:</h4> 8:00 - 18:00</p>
+                                    <p><h4>Wtorek:</h4> 8:00 - 18:00</p>
+                                    <p><h4>Środa:</h4> 8:00 - 18:00</p>
+                                    <p><h4>Czwartek:</h4> 8:00 - 18:00</p>
+                                    <p><h4>Piątek:</h4> 8:00 - 18:00</p>
+                                    <p><h4>Sobota:</h4> 8:00 - 18:00</p>
+                                </div>
+                                <div className="content__right">
+                                    <h3>ul. Sienkiewicza 53</h3>
+                                    <h3>tel. 41 38 40 400</h3>
+                                </div>
+                            </div>
+                            <iframe className="contact__map" title="oddzialSlomniki" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.268326255891!2d20.008755822243096!3d50.35688891743078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4717abd2d68f6cb7%3A0x532dfc1aa9c8ecf!2sHenryka%20Sienkiewicza%2053%2C%2032-200%20Miech%C3%B3w!5e0!3m2!1spl!2spl!4v1590180766079!5m2!1spl!2spl" />
+                        </div>
+                    </div>
+                </div>
                 <Footer />
             </div>
         )
