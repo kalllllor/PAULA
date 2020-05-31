b7b2cac20333a0079a5d6676c840f5f5bab806ac
Mrozuu <58520713+Mrozuu@users.noreply.github.com>
2020-05-31 14:10:17 +0200

    Zmiana menu
    Propozycja zmiany małej koncepcji w hover w menu.
    Ponadto w poprzednim pushu w opisie commita zamiast contact oczywiście producers

--- paula/src/components/menu.js
+++ paula/src/components/menu.js
@@ -14,25 +14,25 @@ class Menu extends React.Component {
                         <Link to="/"><img src={logo} alt="logo" /></Link>
                     </div>
                     <div className="Menu__links">
-                        <Link className={"link" + (this.props.active == "mainPage" ? " link__active" : "") + (this.props.before == "mainPage" ? " link__hide" : "")} 
+                        <Link className={"link" + (this.props.active == "mainPage" ? " link__show" : "") + (this.props.before == "mainPage" ? " link__hide" : "")} 
                             to={{
                                 pathname:"/",
                                 before: this.props.active
                             }}>Strona Główna
                         </Link>
-                        <Link className={"link" + (this.props.active == "aboutPage" ? " link__active" : "") + (this.props.before == "aboutPage" ? " link__hide" : "")} 
+                        <Link className={"link" + (this.props.active == "aboutPage" ? " link__show" : "") + (this.props.before == "aboutPage" ? " link__hide" : "")} 
                             to={{
                                 pathname:"/about",
                                 before: this.props.active
                             }}>O nas
                         </Link>
-                        <Link className={"link" + (this.props.active == "producersPage" ? " link__active" : "") + (this.props.before == "producersPage" ? " link__hide" : "")} 
+                        <Link className={"link" + (this.props.active == "producersPage" ? " link__show" : "") + (this.props.before == "producersPage" ? " link__hide" : "")} 
                             to={{
                                 pathname:"/producers",
                                 before: this.props.active
                             }}>Producenci
                         </Link>
-                        <Link className={"link" + (this.props.active == "contactPage" ? " link__active" : "") + (this.props.before == "contactPage" ? " link__hide" : "")} 
+                        <Link className={"link" + (this.props.active == "contactPage" ? " link__show" : "") + (this.props.before == "contactPage" ? " link__hide" : "")} 
                             to={{
                                 pathname:"/contact",
                                 before: this.props.active
